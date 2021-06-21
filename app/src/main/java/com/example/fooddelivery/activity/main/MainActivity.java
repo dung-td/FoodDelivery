382d4193194f51262a5eee494d728b8a26fcd06d
DucDung_Tong <tngcdng@gmail.com>
2021-06-21 21:16:55 +0700

    Add Place Picker

--- app/src/main/java/com/example/fooddelivery/activity/main/MainActivity.java
+++ app/src/main/java/com/example/fooddelivery/activity/main/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.fooddelivery.activity.main;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.MenuItem;
@@ -9,6 +10,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -25,10 +27,15 @@ import com.example.fooddelivery.model.CallBackData;
 import com.example.fooddelivery.model.ModifyFirebase;
 import com.example.fooddelivery.model.Product;
 import com.example.fooddelivery.model.User;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
@@ -44,7 +51,8 @@ import java.util.Locale;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final int PLACE_PICKER_REQUEST = 1;
+    FloatingActionButton bt_location;
     public static BottomNavigationView bottomNav;
 
     boolean doubleBackToExitPressedOnce = false;
@@ -76,6 +84,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void initView () {
+        bt_location = findViewById(R.id.locationButton);
         bottomNav = findViewById(R.id.bottomNavigationView);
         bottomNav.setBackground(null);
         bottomNav.getMenu().getItem(2).setEnabled(false);
@@ -83,6 +92,34 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                 new HomeFragment()).commit();
+
+        bt_location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+                try {
+                    startActivityForResult(builder.build(MainActivity.this), PLACE_PICKER_REQUEST);
+                } catch (GooglePlayServicesRepairableException e) {
+                    e.printStackTrace();
+                } catch (GooglePlayServicesNotAvailableException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PLACE_PICKER_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlacePicker.getPlace(data, this);
+                StringBuilder builder = new StringBuilder();
+                String latitute = String.valueOf(place.getLatLng().latitude);
+                String longtitue = String.valueOf(place.getLatLng().longitude);
+            }
+        }
     }
 
     private void getData() {
@@ -128,6 +165,4 @@ public class MainActivity extends AppCompatActivity {
             return "en";
         return "vi";
     }
-
-
 }
